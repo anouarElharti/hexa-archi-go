c69d2e9660589bbe2fb7d1b81540752a0183e866
Anouar EL HARTI <B517AS@login.axa>
2024-04-30 17:34:11 +0200

    setting up the project

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import ("github.com/anouarElharti/hexa-archi-go/internal/adapters/core/arithmetic")
+
+func main() {
+	arithmAdapter := arithmetic.NewAdapter()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/anouarElharti/hexa-archi-go
+
+go 1.22.2
